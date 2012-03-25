34dba17a662f37509a4bf58cbd0c04d5683415eb
noman <isnoman2000@yahoo.com>
2012-03-25 09:45:05 +0600

    New file added for Idea Module

---
+++ src/RndBox/Bundle/AuthBundle/Controller/IdeaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace RndBox\Bundle\AuthBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+use RndBox\Bundle\AuthBundle\Entity\IdeaForm;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use RndBox\Bundle\AuthBundle\Entity\Ideas;
+
+class IdeaController extends Controller
+{
+    
+    /**
+     * @Template()
+     */
+    public  function indexAction( Request $request ){
+        $signup = new IdeaForm();
+
+        $form = $this->createFormBuilder( $signup )
+                     ->add('title', 'text',     array('label' => 'Title'))
+                     ->add('slug' , 'text',     array('label' => 'Slug'))
+                     ->add('description',      'textarea',     array('label' => 'Description'))
+                     ->getForm();
+
+        if( $request->getMethod() == 'POST' ){
+            $form->bindRequest($request);
+
+            if( $form->isValid() ){
+                $formData       = $form->getData();
+				$newIdeaId      = $this->processIdeaData($formData);
+                $this->redirect($this->generateUrl('RndBoxAuthBundle_success', array()) );
+            }
+        }
+
+        return array( 'title' => "Idea", 'form' =>$form->createView() );
+    }
+
+    /**
+     * @Template()
+     */
+    public function successAction(){
+        return array( 'success' => 'Idea Posted Successfully' );
+    }
+
+    private function processIdeaData($formData)
+    {
+        $userObj = new Ideas();
+        $userObj->setTitle($formData->getTitle());
+        $userObj->setSlug($formData->getSlug());
+		$userObj->setDescription($formData->getDescription());
+        $docObj = $this->getDoctrine()->getEntityManager();
+        $docObj->persist($userObj);
+        $docObj->flush();
+
+        return $userObj->getIdeaId();
+    }
+ 
+
+}
