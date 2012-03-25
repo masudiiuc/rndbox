88669f29845cc184044efcaec3afb2e6f3836a0e
Mahabubul Hasan Masud <masudiiuc@gmail.com>
2012-03-25 18:27:35 +0600

    Integrated FOSUserBundle.

--- src/RndBox/Bundle/AuthBundle/Entity/Users.php
+++ src/RndBox/Bundle/AuthBundle/Entity/Users.php
@@ -2,6 +2,7 @@
 
 namespace RndBox\Bundle\AuthBundle\Entity;
 
+use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -10,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="users")
  * @ORM\Entity
  */
-class Users
+class Users extends BaseUser
 {
     /**
      * @var integer $userId
@@ -19,101 +20,7 @@ class Users
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    private $userId;
-
-    /**
-     * @var string $emailAddress
-     *
-     * @ORM\Column(name="email_address", type="string", length=255, nullable=false)
-     */
-    private $emailAddress;
-
-    /**
-     * @var string $password
-     *
-     * @ORM\Column(name="password", type="string", length=255, nullable=false)
-     */
-    private $password;
-
-    /**
-     * @var string $userType
-     *
-     * @ORM\Column(name="user_type", type="string", length=100, nullable=false)
-     */
-    private $userType;
-
-    /**
-     * @var datetime $createdDate
-     *
-     * @ORM\Column(name="created_date", type="datetime", nullable=true)
-     */
-    private $createdDate;
-
-    /**
-     * @var datetime $modifiedDate
-     *
-     * @ORM\Column(name="modified_date", type="datetime", nullable=true)
-     */
-    private $modifiedDate;
+    protected $userId;
 
 
-    public function setCreatedDate($createdDate)
-    {
-        $this->createdDate = $createdDate;
-    }
-
-    public function getCreatedDate()
-    {
-        return $this->createdDate;
-    }
-
-    public function setEmailAddress($emailAddress)
-    {
-        $this->emailAddress = $emailAddress;
-    }
-
-    public function getEmailAddress()
-    {
-        return $this->emailAddress;
-    }
-
-    public function setModifiedDate($modifiedDate)
-    {
-        $this->modifiedDate = $modifiedDate;
-    }
-
-    public function getModifiedDate()
-    {
-        return $this->modifiedDate;
-    }
-
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
-
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    public function setUserId($userId)
-    {
-        $this->userId = $userId;
-    }
-
-    public function getUserId()
-    {
-        return $this->userId;
-    }
-
-    public function setUserType($userType)
-    {
-        $this->userType = $userType;
-    }
-
-    public function getUserType()
-    {
-        return $this->userType;
-    }
 }
